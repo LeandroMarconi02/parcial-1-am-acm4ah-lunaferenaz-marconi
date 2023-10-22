e85badce0d6e05e113ea4c1e6cf4d211d1377e7a
LeandroMarconi02 <148662184+LeandroMarconi02@users.noreply.github.com>
2023-10-22 05:05:14 -0300

    navigation bottom 2.0

--- app/src/main/java/ar/edu/parcial_1_am_acm4ah_lunaferenaz_marconi/MainActivity.java
+++ app/src/main/java/ar/edu/parcial_1_am_acm4ah_lunaferenaz_marconi/MainActivity.java
@@ -1,14 +1,48 @@
 package ar.edu.parcial_1_am_acm4ah_lunaferenaz_marconi;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Inicio inicio = new Inicio();
+    Venta venta = new Venta();
+    Inventario inventario = new Inventario();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
+        navigation.setOnItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+    private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public void onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.inicio:
+                    loadFragment(inicio);
+                    return true;
+                case R.id.venta:
+                    loadFragment(venta);
+                    return true;
+                case R.id.inventario:
+                    loadFragment(inventario);
+                    return true;
+
+            }
+
+        }
+    };
+    public void loadFragment(Fragment fragment){
+        Fragment.Transaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.commit();
+
     }
 }
