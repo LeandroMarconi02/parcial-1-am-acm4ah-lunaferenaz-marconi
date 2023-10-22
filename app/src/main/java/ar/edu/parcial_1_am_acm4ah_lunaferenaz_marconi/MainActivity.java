27b5a2e51e33ae745046c45932c73336717b0aed
LeandroMarconi02 <148662184+LeandroMarconi02@users.noreply.github.com>
2023-10-22 12:58:34 -0300

    error en el switch del bottom navigation, ver error

--- app/src/main/java/ar/edu/parcial_1_am_acm4ah_lunaferenaz_marconi/MainActivity.java
+++ app/src/main/java/ar/edu/parcial_1_am_acm4ah_lunaferenaz_marconi/MainActivity.java
@@ -1,24 +1,58 @@
 package ar.edu.parcial_1_am_acm4ah_lunaferenaz_marconi;
+import static ar.edu.parcial_1_am_acm4ah_lunaferenaz_marconi.R.id.inventario;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
+import ar.edu.parcial_1_am_acm4ah_lunaferenaz_marconi.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new InicioFragment());
 
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
 
+            switch (item.getItemId()){
+                case R.id.inicio:
+                    replaceFragment(new InicioFragment());
+                    break;
+                case R.id.venta:
+                    replaceFragment(new VentaFragment());
+                    break;
+                case R.id.inventario:
+                    replaceFragment(new InventoryFragment());
+                    break;
+
+            }
+            return true;
+
+        });
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
 
