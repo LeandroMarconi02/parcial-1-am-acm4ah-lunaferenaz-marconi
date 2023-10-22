5556cf08a362ff2a41351c79c0ebb0fa9dc25279
LeandroMarconi02 <148662184+LeandroMarconi02@users.noreply.github.com>
2023-10-22 01:43:41 -0300

    Initial commit

---
+++ app/src/main/java/ar/edu/parcial_1_am_acm4ah_lunaferenaz_marconi/MainActivity.java
@@ -0,0 +1,14 @@
+package ar.edu.parcial_1_am_acm4ah_lunaferenaz_marconi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
