4715140b7af8298e71f50a6908fb8ab8677068ba
Joaco074 <joaquin.luna@davinci.edu.ar>
2023-10-23 19:36:43 -0300

    feat: added onclick to button

--- app/src/main/java/ar/edu/parcial_1_am_acm4ah_lunaferenaz_marconi/MainActivity.java
+++ app/src/main/java/ar/edu/parcial_1_am_acm4ah_lunaferenaz_marconi/MainActivity.java
@@ -11,6 +11,9 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -19,40 +22,62 @@ import ar.edu.parcial_1_am_acm4ah_lunaferenaz_marconi.databinding.ActivityMainBi
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
-
+    Button btn1;
+//    ActivityMainBinding binding;
+//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        replaceFragment(new InicioFragment());
+        setContentView(R.layout.activity_main);
 
-        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
 
-            switch (item.getItemId()){
-                case R.id.inicio:
-                    replaceFragment(new InicioFragment());
-                    break;
-                case R.id.venta:
-                    replaceFragment(new VentaFragment());
-                    break;
-                case R.id.inventario:
-                    replaceFragment(new InventoryFragment());
-                    break;
 
+        btn1= (Button) findViewById(R.id.btnBuscarLibro);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "", Toast.LENGTH_LONG).show();
             }
-            return true;
-
         });
 
+
+//        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//        replaceFragment(new InicioFragment());
+//
+//        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+//
+//            switch (item.getItemId()){
+//                case R.id.inicio:
+//                    replaceFragment(new InicioFragment());
+//                    break;
+//                case R.id.venta:
+//                    replaceFragment(new VentaFragment());
+//                    break;
+//                case R.id.inventario:
+//                    replaceFragment(new InventoryFragment());
+//                    break;
+//
+//            }
+//            return true;
+//
+//        });
+
+//    }
+////
+////    private void replaceFragment(Fragment fragment){
+////        FragmentManager fragmentManager = getSupportFragmentManager();
+////        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+////        fragmentTransaction.replace(R.id.frame_layout, fragment);
+////        fragmentTransaction.commit();
+//
     }
 
-    private void replaceFragment(Fragment fragment){
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frame_layout, fragment);
-        fragmentTransaction.commit();
+
+
+
+    public void eventoBotonBuscar(View view) {
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
     }
 }
 
